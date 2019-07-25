691bd5094a3e761584945a45cc91517c8ef34f5b
Shreedeep Sawaikar <shreedeep.sawaikar@tcs.com>
2019-07-25 22:01:24 +0200

    Updates

---
+++ precache-manifest.274f70e790627a4358c0de72447458b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ed6ccd1d6835742593ffe68ace71df",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6b50756e8e9e88a84aed",
+    "url": "/monster-rolodex/static/css/main.22a1ef52.chunk.css"
+  },
+  {
+    "revision": "0f1c5798ca0a25facf8c",
+    "url": "/monster-rolodex/static/js/2.d5ed7b26.chunk.js"
+  },
+  {
+    "revision": "6b50756e8e9e88a84aed",
+    "url": "/monster-rolodex/static/js/main.ad5ca774.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
